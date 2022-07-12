e486e719643557c554231b9da93744220be4f2da
Sangram <sangram15.behera@gmail.com>
2022-07-13 02:33:45 +0530

    Local storage

---
+++ js/home.js
@@ -0,0 +1,81 @@
+let empPayrollList;
+window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent= empPayrollList.length;
+    createInnerHtml();
+});
+
+const getEmployeePayrollDataFromStorage=()=>{
+    return localStorage.getItem('EmployeePayrollList') ?
+                        JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
+
+const createInnerHtml=()=>{
+    const headerHtml="<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
+                      "<th>Salary</th><th>Start Date</th><th>Action</th>"
+    if(empPayrollList.length == 0) return;
+    let innerHtml=`${headerHtml}`;
+    //let empPayrollList=createEmployeePayrollJSON();
+    for(const empPayrollData of empPayrollList){
+        innerHtml=`${innerHtml}
+    <tr>
+        <td><img class="profile" alt="" 
+            src="${empPayrollData._profilePic}">
+        </td>
+        <td>${empPayrollData._name}</td>
+        <td>${empPayrollData._gender}</td>
+        <td>${getDeptHtml(empPayrollData._department)}</td>
+        </td>
+        <td>${empPayrollData._salary}</td>
+        <td>${empPayrollData._startDate}</td>
+        <td>
+            <img id="${empPayrollData._id}" onclick="remove(this)" 
+                src="../assets/deleteLogo.png" alt="delete">
+            <img id="${empPayrollData._id}" onclick="update(this)"
+                src="../assets/EditIcon.png"alt="edit">
+        </td>
+    </tr>
+    `;
+    }
+    document.querySelector('#table-display').innerHTML=innerHtml;
+}
+
+const createEmployeePayrollJSON=()=>{
+         let empPayrollListLocal=[
+            {
+                 _name:'Sangram Behera',
+                 _gender:'male',
+                 _department:[
+                     'Engineering',
+                     'Finance'
+                ],
+                 _salary:'500000',
+                 _startDate:'20 Aug 2020',
+                 _note:'Hi This is Me',
+                 _id: new Date().getTime(),
+                 _profilePic: 'C:\Repository\EmployeePayrollApp\assets\profile-images\Ellipse -10.png'
+            },
+            {
+                 _name:'Abhisek Mohanty',
+                 _gender:'female',
+                 _department:[
+                     'Sales'
+            ],
+                 _salary:'400000',
+                 _startDate:'29 Oct 2019',
+                 _note:'i am Abhi',
+                 _id: new Date().getTime() + 1,
+                 _profilePic: 'assets\profile-images\Ellipse -5.png'
+            },
+        ];
+         return empPayrollListLocal;
+}
+
+
+const getDeptHtml=(deptList)=>{
+    let deptHtml='';
+    for(const dept of deptList){
+        deptHtml=`${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
+}
---
+++ js/proporties.js
@@ -0,0 +1,4 @@
+let site_properties = {
+    home_page: "C:\Repository\EmployeePayrollApp\pages\Home.html",
+    add_emp_payroll_page: "C:\Repository\EmployeePayrollApp\pages\payroll_form.html"
+};
